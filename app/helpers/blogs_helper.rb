f6142031f4c98b4b1815a9b4cd989d775611f387
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-09-17 10:05:30 +0900

    BlogHelper内のformat_contentメソッドを修正
    ・sanitizeをview側で行なっていたが、format_contentの処理に含めることとした。

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    html_escape(blog.content).gsub("\n", '<br>')
+    sanitize(html_escape(blog.content).gsub("\n", '<br>'), tags: %w[br])
   end
 end
